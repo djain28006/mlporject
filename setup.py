acc1f527165a0012651c2a4195624bbd64713250
Danish Jain <danishsjain@gmail.com>
2025-11-21 19:35:15 +0530

    logging & exception

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 # with the help of setup.py i can build my entire machine learning project as a package and deploy in pypi
+# It doesn’t run your code, it just provides instructions for installation.
+
 from setuptools import setup, find_packages
 from typing import List
 
@@ -22,6 +24,6 @@ setup(
     version='0.1.0',
     author='Danish Jain',
     author_email='danishsjain@gmail.com',
-    packages =find_packages(),
+    packages =find_packages(),# finds src/ automatically
     install_requires=get_requirements('requirements.txt')
 )
---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+# this src folder is a package from where i can import my codes 
+# and use them in other projects by installing this package via pypi
+# i will use setup.py to build this package and deploy it to pypi
+# it will be used when main.py requires any function from this package
