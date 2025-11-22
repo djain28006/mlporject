11520053b1671ef8127896d2bdfea756daf5bb92
danish <danishsjain@gmail.com>
2025-11-22 18:48:43 +0530

    model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,10 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     raw_data_path: str = os.path.join('artifacts', 'raw_data.csv')
@@ -63,4 +67,7 @@ if __name__ == "__main__":
     train_data,test_data=obj.initiate_data_ingestion(source_file_path='notebook\data\stud.csv')
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    ModelTrainer = ModelTrainer()
+    print(ModelTrainer.initiate_model_trainer(train_arr,test_arr))
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 from src.exception import CustomException
 from src.logger import logging
 import dill
+from sklearn.metrics import r2_score
 
 def save_object(file_path: str, obj):
     try:
@@ -17,4 +18,28 @@ def save_object(file_path: str, obj):
 
     except Exception as e:
         logging.error("Error saving object")
+        raise CustomException(e, sys) from e
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+            model_name = list(models.keys())[i]
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+            logging.info(f"{model_name} R2 Score: {test_model_score}")
+
+        return report
+
+    except Exception as e:
+        logging.error("Error evaluating models")
         raise CustomException(e, sys) from e
